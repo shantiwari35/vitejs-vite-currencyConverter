de2130246ef1accba7e462dcb519fdf1ddc1011e
shantiwari35 <shantiwari35@gmail.com>
2023-09-21 00:22:42 +0530

    4 files modified, 5 added

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+export default function useCurrencyInfo(currency) {
+  let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  return data;
+}
